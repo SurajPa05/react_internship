45827fb493b1ded517e136240f47ccc00ec9ffc9
SurajPa05 <surajpa72@gmail.com>
2026-06-12 10:02:16 +0530

    4

---
+++ js/day.js
@@ -0,0 +1,39 @@
+// 1 - monday
+// 2
+// .
+// .
+// .
+// .
+// .
+// 7 - sunday
+// else - invalid case
+
+op = 4
+
+switch(op){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("wednesday")
+        break
+    case 4:
+        console.log("thurshday")
+        break
+    case 5:
+        console.log("friday")
+        break
+    case 6:
+        console.log("saturday")
+        break
+    case 7:
+        console.log("sunday")
+        break
+    default:
+        console.log("Invalid day")
+        break
+
+}
---
+++ js/login.js
@@ -0,0 +1,17 @@
+var USERNAME = "user"
+var PASSWORD = "12345678"
+
+
+var user = "user"
+var pass = "12345678"
+
+
+if (user === USERNAME){
+    if(pass === PASSWORD){
+        console.log("Login Successfull")
+    }else{
+        console.log("Invalid password")
+    }
+}else{
+    console.log("User not found")
+}
